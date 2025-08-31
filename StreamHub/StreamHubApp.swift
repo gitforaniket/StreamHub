6ace8ebb1150cdefc5d39ac6abb9939f636ed5b2
Aniket <heyaniket@hotmail.com>
2025-08-31 23:54:42 +0530

    Fix: Project Build and chore: add signing methods

--- StreamHub/StreamHubApp.swift
+++ StreamHub/StreamHubApp.swift
@@ -1,17 +1,55 @@
-//
-//  StreamHubApp.swift
-//  StreamHub
-//
-//  Created by Aniket's Mac on 31/08/25.
-//
-
 import SwiftUI
+import WebKit
 
 @main
 struct StreamHubApp: App {
+    init() {
+        // Enable persistent cookies and login sessions
+        HTTPCookieStorage.shared.cookieAcceptPolicy = .always
+        
+        // Configure WebKit settings
+        let dataStore = WKWebsiteDataStore.default()
+        dataStore.httpCookieStore.getAllCookies { cookies in
+            // Cookies are persisted automatically
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
+        }
+        .windowStyle(.titleBar)
+        .windowToolbarStyle(.unified)
+        .commands {
+            // Menu commands
+            CommandGroup(replacing: .newItem) {
+                Button("Add Platform...") {
+                    NotificationCenter.default.post(name: .showAddPlatform, object: nil)
+                }
+                .keyboardShortcut("n", modifiers: .command)
+            }
+            
+            CommandMenu("Navigation") {
+                Button("Back") {
+                    NotificationCenter.default.post(name: .goBack, object: nil)
+                }
+                .keyboardShortcut("[", modifiers: .command)
+                
+                Button("Forward") {
+                    NotificationCenter.default.post(name: .goForward, object: nil)
+                }
+                .keyboardShortcut("]", modifiers: .command)
+                
+                Button("Refresh") {
+                    NotificationCenter.default.post(name: .refresh, object: nil)
+                }
+                .keyboardShortcut("r", modifiers: .command)
+            }
         }
     }
+}
+
+extension Notification.Name {
+    static let showAddPlatform = Notification.Name("showAddPlatform")
 }
